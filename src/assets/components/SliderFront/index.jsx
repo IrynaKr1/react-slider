51e7abf93a5e83353eb6db393d5925f6a21a5798
IrynaKr1 <krutsiryna@gmail.com>
2025-10-19 15:30:34 +0200

    Add img from api

--- src/assets/components/SliderFront/index.jsx
+++ src/assets/components/SliderFront/index.jsx
@@ -1,16 +1,27 @@
 import styles from './SliderFront.module.scss';
 import defaultImg from '../../imgDefault/default.jpg';
 
-function SliderFront() {
-  return (
-    <>
+function SliderFront({ image, isLoading }) {
+  if (isLoading || !image) {
+    return (
       <figure className={styles.sliderContainer}>
-        <img src={defaultImg} alt='Default img' className={styles.sliderImg} />
-        <figcaption className={styles.sliderDescription}>
-          Default img
-        </figcaption>
+        <img
+          src={defaultImg}
+          alt='Loading image...'
+          className={styles.sliderImg}
+        />
+        <figcaption className={styles.sliderDescription}>Loading...</figcaption>
       </figure>
-    </>
+    );
+  }
+
+  return (
+    <figure className={styles.sliderContainer}>
+      <img src={image.imageUrl} alt='Image' className={styles.sliderImg} />
+      <figcaption className={styles.sliderDescription}>Author: 
+        {image.author}
+      </figcaption>
+    </figure>
   );
 }
 
--- src/assets/components/SliderFull/index.jsx
+++ src/assets/components/SliderFull/index.jsx
@@ -1,14 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Slider.module.scss';
 import SliderFront from '../SliderFront';
+import defaultImg from '../../imgDefault/default.jpg';
 
 function Slider() {
+  const [images, setImages] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const fetchingImg = async () => {
+    try {
+      setIsFetching(true);
+      const response = await fetch(
+        'https://picsum.photos/v2/list?page=1&limit=50'
+      );
+      const data = await response.json();
+      const formattedImg = data.map((img) => ({
+        imageUrl: `https://picsum.photos/id/${img.id}/600/400`,
+        author: img.author,
+      }));
+
+      setImages(formattedImg);
+      setError(null);
+    } catch (err) {
+      setError('Failed to load images');
+    } finally {
+      setIsFetching(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchingImg();
+  }, []);
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  if (error) {
+    return <div className={styles.error}>{error}</div>;
+  }
+
   return (
-    <><button>Rigth</button>
-    <SliderFront />
-    <button>Left</button>
-    </>
-  )
+    <div className={styles.sliderWrapper}>
+      <button
+        onClick={handlePrev}
+        disabled={isFetching || images.length === 0}
+        className={styles.btnLeft}
+      >
+        Left
+      </button>
+
+      <SliderFront image={images[currentIndex]} isLoading={isFetching} />
+
+      <button
+        onClick={handleNext}
+        disabled={isFetching || images.length === 0}
+        className={styles.btnRight}
+      >
+        Right
+      </button>
+    </div>
+  );
 }
 
 export default Slider;
